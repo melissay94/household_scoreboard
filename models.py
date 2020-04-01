72d41f11babaabdf7e2a09149f444191663be12a
melissay94 <melissay94@yahoo.com>
2020-03-31 19:00:58 -0700

    Completed and tested models

--- models.py
+++ models.py
@@ -9,17 +9,19 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:postgres@localhos
 db = SQLAlchemy(app)
 
 household_chores = db.Table("household_chores",
-  db.Column("household_id", db.Integer, db.ForiegnKey("households.id"), primary_key=True),
-  db.Column("chore_id", db.Integer, db.ForiegnKey("chores.id"), primary_key=True)
+  db.Column("household_id", db.Integer, db.ForeignKey("households.id"), primary_key=True),
+  db.Column("chore_id", db.Integer, db.ForeignKey("chores.id"), primary_key=True),
+  db.Column("completed", db.Boolean, default=False)
 )
 
-user_chores = db.Table("user_chores",
-  db.Column("user_id", db.Integer, db.ForiegnKey("users.id"), primary_key=True),
-  db.Column("chore_id", db.Integer, db.ForiegnKey("chores.id"), primary_key=True)
+user_chores = db.Table("pending_user_chores",
+  db.Column("user_id", db.Integer, db.ForeignKey("users.id"), primary_key=True),
+  db.Column("chore_id", db.Integer, db.ForeignKey("chores.id"), primary_key=True),
+  db.Column("completed", db.Boolean, default=False)
 )
 
 class Household(db.Model):
-  __tablename__ = "household"
+  __tablename__ = "households"
 
   id = db.Column(db.Integer, primary_key=True)
   name = db.Column(db.String, unique=True, nullable=False)
@@ -27,35 +29,37 @@ class Household(db.Model):
   chores = db.relationship("Chore", 
     secondary=household_chores, 
     lazy="subquery", 
-    backref=db.backref("chores"), 
-    lazy=True
+    backref=db.backref("household_chores")
   )
 
   def __repr__(self):
     return f"Household {self.name}"
   
 class User(db.Model):
-  __tablename__ = "user"
+  __tablename__ = "users"
 
   id = db.Column(db.Integer, primary_key=True)
-  email = db.Colum(db.String, unique=True, nullable=False)
+  email = db.Column(db.String, unique=True, nullable=False)
   name = db.Column(db.String(25), nullable=False)
-  household_id = db.Column(db.Integer, db.ForiegnKey("households.id", ondelete="SET NULL"))
+  score = db.Column(db.Integer, nullable=False, default=0)
+  household_id = db.Column(db.Integer, db.ForeignKey("households.id", ondelete="SET NULL"))
   chores = db.relationship("Chore", 
-    secondary=household_chores, 
+    secondary=user_chores, 
     lazy="subquery", 
-    backref=db.backref("chores"), 
-    lazy=True
+    backref=db.backref("user_chores")
   )
 
   def __repr__(self):
     return f"User {self.name} has email {self.email} and is in household {self.household_id}"
 
 class Chore(db.Model):
-  __tablename__ = "chore"
+  __tablename__ = "chores"
 
   id = db.Column(db.Integer, primary_key=True)
   name = db.Column(db.String, unique=True, nullable=False)
   description = db.Column(db.String(180))
   difficulty = db.Column(db.Integer, nullable=False)
-  
+
+  def __repr__(self):
+    return f"Chore {self.name} is {self.description} and has a {self.difficulty} rating"
+
